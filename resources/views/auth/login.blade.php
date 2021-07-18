b41ae928f1d36c4896e0acb3e779cc4e3c401bb3
Midlaj <midlajmundath@gmail.com>
2021-07-18 23:14:39 +0530

    login design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <!-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
@@ -68,6 +68,104 @@
                 </div>
             </div>
         </div>
+    </div> -->
+
+
+    <!-- Page content -->
+    <div class="page-content">
+
+        <!-- Main content -->
+        <div class="content-wrapper">
+
+            <!-- Content area -->
+            <div class="content d-flex justify-content-center align-items-center">
+
+                <!-- Login form -->
+                 <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                <!-- <form class="login-form" > -->
+                    <div class="card mb-0">
+                        <div class="card-body">
+                            <div class="text-center mb-3">
+                                <i class="icon-user icon-2x text-slate-300 border-slate-300 border-3 rounded-round p-3 mb-3 mt-1"></i>
+                                <h5 class="mb-0">Login as Student</h5>
+                                <span class="d-block text-muted">Your credentials</span>
+                            </div>
+
+                            <div class="form-group form-group-feedback form-group-feedback-left">
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}"  placeholder="Username" required autocomplete="email" autofocus>
+                                <div class="form-control-feedback">
+                                    <i class="icon-user text-muted"></i>
+                                </div>
+
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+                            </div>
+
+                            <div class="form-group form-group-feedback form-group-feedback-left">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" class="form-control @error('password') is-invalid @enderror">
+                                <div class="form-control-feedback">
+                                    <i class="icon-lock2 text-muted"></i>
+                                </div>
+
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group d-flex align-items-center">
+                                <div class="form-check mb-0">
+                                    <label class="form-check-label">
+                                        <input type="checkbox" name="remember" class="form-input-styled" checked data-fouc>
+                                        Remember
+                                    </label>
+                                </div>
+                                <a href="password_recover.html" class="ml-auto">Forgot password?</a>
+                            </div>
+
+                            <div class="form-group">
+                                <a href="dashboard.html">
+                                    <button type="submit" class="btn btn-primary btn-block">
+                                        Sign in 
+                                        <i class="icon-circle-right2 ml-2"></i>
+                                    </button>
+                                </a>
+                            </div>
+
+                            <div class="form-group text-center text-muted content-divider">
+                                <span class="px-2">or sign in with</span>
+                            </div>
+
+                            <div class="form-group text-center">
+                                <button type="button" class="btn btn-outline bg-pink-300 border-pink-300 text-pink-300 btn-icon rounded-round border-2"><i class="icon-google"></i></button>
+                                <button type="button" class="btn btn-outline bg-indigo border-indigo text-indigo btn-icon rounded-round border-2 ml-2"><i class="icon-facebook"></i></button>
+                                <button type="button" class="btn btn-outline bg-slate-600 border-slate-600 text-slate-600 btn-icon rounded-round border-2 ml-2"><i class="icon-envelope"></i></button>
+                            </div>
+
+                            <div class="form-group text-center text-muted content-divider">
+                                <span class="px-2">Don't have an account?</span>
+                            </div>
+
+                            <div class="form-group">
+                                <a href="registration.html" class="btn btn-light btn-block">Sign up</a>
+                            </div>
+
+                            <span class="form-text text-center text-muted">By continuing, you're confirming that you've read our <a href="#">Terms &amp; Conditions</a> and <a href="#">Cookie Policy</a></span>
+                        </div>
+                    </div>
+                </form>
+                <!-- /login form -->
+            </div>
+            <!-- /content area -->
+        </div>
+        <!-- /main content -->
     </div>
+    <!-- /page content -->
 </div>
 @endsection
