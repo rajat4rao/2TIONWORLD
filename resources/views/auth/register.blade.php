94a68a7367cbb6f9094c0432f854e4ec06e6a68e
Midlaj <midlajmundath@gmail.com>
2021-07-18 15:51:41 +0530

    Registration design

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -206,5 +206,226 @@
             </div>
         </div>
     </div>
+</div> -->
+
+
+
+<!-- Registration Design -->
+<!-- Page content -->
+    <div class="page-content">
+
+        <!-- Main content -->
+        <div class="content-wrapper">
+
+            <!-- Content area -->
+            <div class="content d-flex justify-content-center align-items-center">
+
+                <!-- Registration form -->
+                <form action="index.html" class="flex-fill">
+                    <div class="row">
+                        <div class="col-lg-6 offset-lg-3">
+                            <div class="card mb-0">
+                                <div class="card-body">
+                                    <div class="text-center mb-3">
+                                        <i class="icon-plus3 icon-2x text-success border-success border-3 rounded-round p-3 mb-3 mt-1"></i>
+                                        <h5 class="mb-0">Create account</h5>
+                                        <span class="d-block text-muted">All fields are required</span>
+                                    </div>
+
+                                    <form method="POST" action="{{ action('Auth\RegisterController@storeRegistrationForm') }}">
+                                    @csrf
+
+
+                                    <div class="form-group form-group-feedback form-group-feedback-right">
+                                        
+                                
+                                        <input type="text" class="form-control @error('username') is-invalid @enderror" placeholder="Choose username" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus="">
+                                        <div class="form-control-feedback">
+                                            <i class="icon-user-plus text-muted"></i>
+                                        </div>
+                                        @error('username')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-feedback form-group-feedback-right">
+                                                <input id="firstname" type="text" class="form-control @error('name') is-invalid @enderror" name="firstname" value="{{ old('firstname') }}" required autocomplete="name" autofocus placeholder="First Name">
+                                                <div class="form-control-feedback">
+                                                    <i class="icon-user-check text-muted"></i>
+                                                </div>
+
+                                                @error('name')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-feedback form-group-feedback-right">
+                                                <input id="secondname" type="text" class="form-control @error('secondname') is-invalid @enderror" name="secondname" value="{{ old('secondname') }}" required autocomplete="secondname" autofocus placeholder="Second Name">
+                                                <div class="form-control-feedback">
+                                                    <i class="icon-user-check text-muted"></i>
+                                                </div>
+
+                                                 @error('secondname')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-feedback form-group-feedback-right">
+                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="password" autofocus placeholder="Password">
+
+                                                <div class="form-control-feedback">
+                                                    <i class="icon-user-lock text-muted"></i>
+                                                </div>
+
+                                                 @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-feedback form-group-feedback-right">
+                                              
+
+                                                <input id="confirm-password" type="password" class="form-control @error('c-password') is-invalid @enderror" name="confirm-password" value="{{ old('confirm-password') }}" required autocomplete="confirm-password" autofocus placeholder="Confirm Password">
+                                                <div class="form-control-feedback">
+                                                    <i class="icon-user-lock text-muted"></i>
+                                                </div>
+
+                                                @error('confirm-password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-feedback form-group-feedback-right">
+                                                
+                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-Mail">
+
+
+                                                <div class="form-control-feedback">
+                                                    <i class="icon-mention text-muted"></i>
+                                                </div>
+
+                                                 @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-feedback form-group-feedback-right">
+
+                                                <input id="mobilenumber" type="text" class="form-control @error('e') is-invalid @enderror" name="mobilenumber" value="{{ old('mobilenumber') }}" required autocomplete="mobilenumber" autofocus placeholder="Mobile Number">
+
+
+                                                <div class="form-control-feedback">
+                                                    <i class="icon-mention text-muted"></i>
+                                                </div>
+
+                                                @error('mobilenumber')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror    
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group form-group-feedback form-group-feedback-right">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="role" id="tutor">
+                                         <label class="form-check-label" for="tutor">
+                                            Tutor
+                                          </label>    
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="role" id="student" checked>
+                                        <label class="form-check-label" for="student">
+                                            Student
+                                          </label>    
+                                        </div>
+                                          
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input type="checkbox" class="form-input-styled" data-fouc>
+                                                Accept <a href="#" data-toggle="modal" data-target="#exampleModalLong">terms of service</a>
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn bg-teal-400 btn-labeled btn-labeled-right"><b><i class="icon-plus3"></i></b> Create account</button>
+                                </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <!-- /registration form -->
+
+            </div>
+            <!-- /content area -->
+
+
+
+        </div>
+        <!-- /main content -->
+
+    </div>
+    <!-- /page content -->
+<!-- /Registration Design -->
+
+
+
+<!-- Modal -->
+<!-- Modal -->
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        djksdfn asdfjaskdfjas fdlaskdjf laskjdf aslkdfj laksjdf laskjdf alskdjfa slkdjf laskdjf lkasdjf lsakdjf aslkdfjalskdjf laksdjf lkasdjf aslkd
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
+
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,28 @@
         }
     </style>
 
+    <!-- Global stylesheets -->
+    <link href=" {{ asset('css/global_assets/css/icons/icomoon/styles.min.css') }}" rel="stylesheet" type="text/css">
+    <!-- <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"> -->
+    <link href="{{ asset('css/assets/css/bootstrap_limitless.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/assets/css/layout.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/assets/css/components.min.css') }}" r') }}el="stylesheet" type="text/css">
+    <link href="{{ asset('css/assets/css/colors.min.css') }}" rel="stylesheet" type="text/css">
+    <!-- /global stylesheets -->
+
+    <!-- Core JS files -->
+    <script src="{{ asset('css/global_assets/js/main/jquery.min.js') }}"></script>
+    <script src="{{ asset('css/global_assets/js/main/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('css/global_assets/js/plugins/loaders/blockui.min.js') }}"></script>
+    <!-- /core JS files -->
+
+    <!-- Theme JS files -->
+    <script src="{{ asset('css/global_assets/js/plugins/forms/styling/uniform.min.js') }}"></script>
+
+    <script src="{{ asset('css/assets/js/app.js') }}"></script>
+    <script src="{{ asset('css/global_assets/js/demo_pages/login.js') }}"></script>
+    <!-- /theme JS files -->
+
          
 </head>
      
